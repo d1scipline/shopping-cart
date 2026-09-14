1e7a7ddca95995993e110d3035ba5284cc66a181
d1scipline <143661704+d1scipline@users.noreply.github.com>
2026-09-14 18:06:06 +0300

    feat: fetch data from fakestore API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const url = "https://fakestoreapi.com/products";
 
-  return <h1>Hello world!</h1>;
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw new Error("HTTP error! Status: " + response.status);
+        }
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <h1>Loading</h1>;
+  }
+
+  if (error) {
+    return <h1>Error!</h1>;
+  }
+
+  return (
+    <ul>
+      {data.map((item) => (
+        <li key={item.id}>{item.title}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
